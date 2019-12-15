04c781de1c5b3e117d846c5d51c1b7e335ffbbbd
Sergey Shelekhov <sheleh121@gmail.com>
2019-12-15 15:36:47 +0700

    delete css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -54,20 +54,8 @@
     <script src="{{ asset('js/main.js') }}" defer></script>
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-    <!--
-    CSS
-    ============================================= -->
-    <link rel="stylesheet" href="{{ asset('css/linearicons.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+    
     @yield('head')
 </head>
 <body style="display: block;">
