35e8d06518dc449f906008af5fe7cfcd853e2bea
Sergey Shelekhov <sheleh121@gmail.com>
2019-12-15 15:37:04 +0700

    delete css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -55,7 +55,7 @@
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    
+
     @yield('head')
 </head>
 <body style="display: block;">
